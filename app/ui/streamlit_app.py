428320d45b2284d3c95b3ba09e6c33192e94b2ca
apassan <annie.passan@gmail.com>
2025-06-26 21:29:00 +0200

    fix file paths

--- app/ui/streamlit_app.py
+++ app/ui/streamlit_app.py
@@ -11,11 +11,11 @@ import os
 from app.config import REPO_ROOT
 
 # Go 2 levels up to add the repo root to sys.path
-# This needs to happeb n before importing any modules from the repo
+# This needs to happen before importing any modules from the repo
 current_dir = os.path.dirname(__file__)
-#repo_root = os.path.abspath(os.path.join(current_dir, "../../"))
-if REPO_ROOT not in sys.path:
-    sys.path.insert(0,  REPO_ROOT)
+repo_root = os.path.abspath(os.path.join(current_dir, "../../"))
+if repo_root not in sys.path:
+    sys.path.insert(0, repo_root)
     print("✅ sys.path:", sys.path)
 
 import streamlit as st
