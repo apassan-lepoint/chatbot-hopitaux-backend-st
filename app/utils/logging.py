3c796de3e3af088df1af43c1e97b89a0284436f4
apassan <annie.passan@gmail.com>
2025-07-05 14:08:16 +0200

    processing

--- app/utils/logging.py
+++ app/utils/logging.py
@@ -13,7 +13,7 @@ LOG_FILE = os.path.join(os.path.dirname(__file__), "app.log")
 # - Define the log message format
 # - Log messages will be written both to a file and to the console
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     handlers=[
         logging.FileHandler(LOG_FILE, encoding="utf-8"),
