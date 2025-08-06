1ad5f1d0bcfc236226be5c80b2c72e134ac021e6
Anuradha Passan <apassan@lp-1230.lepoint.fr>
2025-08-06 15:56:40 +0200

    decouple multiturn logic

--- app/config/features_config.py
+++ app/config/features_config.py
@@ -59,7 +59,7 @@ STATUS_DESCRIPTIONS_DICT = {
 
 
 # Constants for checks
-MAX_MESSAGES = 2
+MAX_MESSAGES = 1
 MAX_LENGTH = 200
 ENABLE_MULTI_TURN = False  # Set to False to disable multiturn logic
 
